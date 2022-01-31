12825f8f48bd4f9a3f0ef212a0ba9537786cee67
Stanislav <staniskudriavtsev@yandex.ru>
2022-01-31 20:30:19 +0300

    exams lvl03: paramsum

---
+++ exams/level03/paramsum/paramsum.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 20:28:07 by stanislav         #+#    #+#             */
+/*   Updated: 2022/01/31 20:28:07 by stanislav        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		if (nb <= -10)
+			ft_putnbr(nb / -10);
+		ft_putchar(-(nb % 10) + '0');
+	}
+	else
+	{
+		if (nb >= 10)
+			ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
+	}
+}
+
+int	main(int ac, char **av)
+{
+	(void) av;
+
+	ft_putnbr(ac - 1);
+	ft_putchar('\n');
+	return (0);
+}
