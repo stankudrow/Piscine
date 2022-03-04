3250f68290f8dca9b9ec0b2b0f03d78a40e79fee
Stanislav <staniskudriavtsev@yandex.ru>
2022-03-04 23:06:13 +0300

    inter fixed

--- exams/level02/inter/inter.c
+++ exams/level02/inter/inter.c
@@ -6,11 +6,21 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 10:54:28 by stanislav         #+#    #+#             */
-/*   Updated: 2022/01/08 10:54:29 by stanislav        ###   ########.fr       */
+/*   Updated: 2022/03/04 23:04:35 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
 
 // find a character c in the first n bytes of a string s
 char	*ft_strnchr(char *s, char c, size_t n)
@@ -27,20 +37,26 @@ char	*ft_strnchr(char *s, char c, size_t n)
 
 int	main(int ac, char **av)
 {
-	size_t	c1;
-	size_t	c2;
+	char	*s1;
+	char	*s2;
+	size_t	len2;
 
 	if (ac == 3)
 	{
-		c1 = 0;
-		c2 = 0;
-		while (av[1][c1] && av[2][c2])
+		s1 = av[1];
+		s2 = av[2];
+		len2 = ft_strlen(s2);
+		while (*s1 && *s2)
 		{
-			while (av[2][c2] != av[1][c1])
-				c2++;
-			if (av[2][c2] && !ft_strnchr(av[1], av[1][c1], c1))
-				write(1, &av[2][c2], 1);
-			c1++;
+			if (!ft_strnchr(av[1], *s1, s1 - av[1]))
+			{
+				s2 = ft_strnchr(s2, *s1, len2);
+				if (s2)
+					write(1, s2, 1);
+				else
+					s2 = av[2];
+			}
+			s1++;
 		}
 	}
 	write(1, "\n", 1);
